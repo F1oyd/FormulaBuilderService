7ca066bd60a37b61ec3825951270f9fc9c884b64
Victor Korzunin <ifloydrose@gmail.com>
2017-11-11 23:45:45 +0500

    Implement WPF client demo app

---
+++ FBS.WpfClient/ConnectionStatusViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBS.WpfClient
+{
+    public sealed class ConnectionStatusViewModel : INotifyPropertyChanged
+    {
+        public ConnectionStatusViewModel()
+        {
+            _connectionButtonTitle = "Connect";
+            _requestString = @"<request>
+    <expression>
+    <operation>plus</operation>
+    <operand>
+        <const>20</const>
+    </operand>
+    <operand>
+        <expression>
+            <operation>minus</operation>
+            <operand>
+                <const>10</const>
+            </operand>
+            <operand>
+                <const>5</const>
+            </operand>
+        </expression>
+    </operand>
+    </expression>
+</request>";
+        }
+
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set
+            {
+                _isConnected = value;
+                OnPropertyChanged();
+                ConnectionButtonTitle = _isConnected ? "Disconnect" : "Connect";
+            }
+        }
+
+        private string _connectionButtonTitle;
+        public string ConnectionButtonTitle
+        {
+            get
+            {
+                return _connectionButtonTitle;
+            }
+            set
+            {
+                _connectionButtonTitle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _requestString;
+        public string RequestString
+        {
+            get
+            {
+                return _requestString;
+            }
+            set
+            {
+                _requestString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _responseString;
+        public string ResponseString
+        {
+            get
+            {
+                return _responseString;
+            }
+            set
+            {
+                _responseString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FBS.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace FBS.WpfClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this._viewModel = new ConnectionStatusViewModel();
+            this.DataContext = this._viewModel;
+        }
+
+        private readonly ConnectionStatusViewModel _viewModel;
+
+        private string Host => tbHost.Text;
+
+        private int Port => int.TryParse(tbPort.Text, out int port) ? port : 0;
+
+        private TcpClient _client;
+
+        private void ConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_viewModel.IsConnected)
+            {
+                try
+                {
+                    _client = new TcpClient(Host, Port);
+                    _viewModel.IsConnected = true;
+                }
+                catch (SocketException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+            else
+            {
+                SendMessage("DisconnectEvent");
+                _client.Close();
+                _viewModel.IsConnected = false;
+            }
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(_viewModel.RequestString);
+
+            _viewModel.ResponseString = ReceiveMessage();
+        }
+
+        private void SendMessage(string message)
+        {
+            byte[] requestBytes = Encoding.UTF8.GetBytes(message);
+            _client.GetStream().Write(requestBytes, 0, requestBytes.Length);
+        }
+
+        private string ReceiveMessage()
+        {
+            byte[] buffer = new byte[102400];
+            int i = _client.GetStream().Read(buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer, 0, i);
+        }
+    }
+}
